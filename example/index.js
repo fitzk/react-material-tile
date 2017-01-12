29a1bbb5db7b08bca367d6f930b0607d3bba064b
fitzk <kayla.fitzsimmons@protonmail.com>
2017-01-12 10:49:51 -0800

    add trivial example and update example/package.json to run build and dev scripts

---
+++ example/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react"
+import { AppContainer } from "react-hot-loader"
+import { render } from "react-dom"
+import injectTapEventPlugin from "react-tap-event-plugin"
+import App from "source/app"
+
+injectTapEventPlugin()
+
+const renderApp = () => {
+  render(
+    <AppContainer>
+      <App />
+    </AppContainer>, document.getElementById( "app" )
+  );
+}
+
+renderApp() // Renders App on init
+
+if ( module.hot ) {
+  module.hot.accept( "./app", renderApp )
+}
