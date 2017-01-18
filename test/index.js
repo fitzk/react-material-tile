44c0048a29bdb3a0e982d8734be8348f753c9f3c
fitzk <kayla.fitzsimmons@protonmail.com>
2017-01-18 15:36:16 -0800

    update test for tile-card-uncontrolled instead of controlled

--- test/index.js
+++ test/index.js
@@ -1,50 +1,27 @@
-/**
-* @Author: Kayla Fitzsimmons <kaylafitzsimmons>
-* @Date:   01-08-2017
-* @Email:  kayla.fitzsimmons@protonmail.com
-* @Project: tile-card
-* @Filename: index.js
-* @Last modified by:   fitzk
-* @Last modified time: 01-09-2017
-* @License: MIT
-* @Copyright: 2016-present
-*/
-
 import React from "react"
 import { test } from "ava"
 import { mount, shallow } from "enzyme"
-import { Stateless, Stateful } from "./../distribution/react-tilecard.js"
+import TileCard from "./../distribution/react-tilecard.js"
 import { noop, renderJSX, JSX } from "jsx-test-helpers"
 
-/*
-Stateless Component PropTypes
-    name: string,            - required prop
-    uniqueKey: string,       - required prop
-    expanded: bool,          - required prop
-    onExpandChange: func,    - required prop
-    initiallyExpanded: bool, - required prop
-    avatarSrc: any,          - required prop
 
-    title: string,
-    subtitle: string,
-    childContainerClassName: string,
-    tileClassName: string,
-    headerClassName: string,
-    containerClassName: string,
-    children: any
-*/
-
-test('Stateless: exists', t => {
+test('TileCard: exists', t => {
 	const expanded = false
-	const wrapper = shallow(<Stateless expanded={ expanded }
-                                    title="Example"
-                                    avatarSrc="file.jpg"
-                                    initiallyExpanded={ false }
-                                    name="example"
-                                    childContainerClassName="child-container"
-                                    containerClassName="container"
-                                    uniqueKey="example"
-                                    onExpandChange={ () => {
-                                                     } } />)
-	t.truthy(wrapper)
+	const onExpandChange = () => true
+	const wrapper = shallow(<TileCard showAvatarOnExpand
+                                   title="Tile Card"
+                                   subtitle={ "a React Component" }
+                                   initiallyExpanded={ false }
+                                   cardClassName
+                                   headerClassName
+                                   tileWidth
+                                   tileHeight
+                                   avatarWidth
+                                   avatarHeight
+                                   name="name">
+                           <div>
+                             Hello
+                           </div>
+                         </TileCard>)
+	t.truthy(wrapper.exists())
 })
