0439327e022b74b4ca270fe26b43460536ad1a9e
fitzk <kayla.fitzsimmons@protonmail.com>
2017-01-22 17:43:29 -0800

    small update to example

--- example/source/app.js
+++ example/source/app.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import { AppBar } from "material-ui";
 import getMuiTheme from "material-ui/styles/getMuiTheme";
-import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import "./assets/index.scss"
+import darkBaseTheme from "material-ui/styles/baseThemes/darkBaseTheme";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import { TileCard } from "./source/tile-card"
+import { ThemeProvider } from "styled-components"
 import { SDKS } from "./sdks"
 
 export class App extends Component {
@@ -15,13 +16,15 @@ export class App extends Component {
 		const muiTheme = getMuiTheme(darkBaseTheme);
 		return <div>
            <MuiThemeProvider muiTheme={ muiTheme }>
-             <div>
-               <AppBar showMenuIconButton={ false }
-                       title="FAUX"
-                       titleStyle={ { color: "white", fontSize: isMobile ? "8rem" : "auto", marginTop: ".5em" } }
-                       style={ { backgroundColor: "black", marginBottom: "8em", height: isMobile ? "10em" : "auto" } } />
-               <SDKS/>
-             </div>
+             <ThemeProvider theme={ darkBaseTheme }>
+               <div>
+                 <AppBar showMenuIconButton={ false }
+                         title="FAUX"
+                         titleStyle={ { color: "white", fontSize: isMobile ? "8rem" : "auto", marginTop: ".5em" } }
+                         style={ { backgroundColor: "black", marginBottom: "8em", height: isMobile ? "10em" : "auto" } } />
+                 <SDKS/>
+               </div>
+             </ThemeProvider>
            </MuiThemeProvider>
          </div>
 	}
--- example/source/sdk-detail.js
+++ example/source/sdk-detail.js
@@ -1,28 +1,18 @@
 import React from "react"
-import "./assets/index.scss"
 import TileCard from "react-tilecard"
 
 export const SDKDetail = props => {
 	const isMobile = /iPhone|iPad|iPod|Android|Mobile/i.test(navigator.userAgent);
 
 	let square = "150px"
-	let smallSquare = "40px"
 	if (isMobile) {
 		square = "360px"
-		smallSquare = "80px"
 	}
 
-	return <TileCard hideAvatarOnExpand={ false }
-                  title={ props.title }
-                  initiallyExpanded={ false }
+	return <TileCard title={ props.title }
                   avatarSrc={ props.avatarSrc }
-                  backgroundColor="#F3E5F5"
-                  tileWidth={ square }
-                  tileHeight={ square }
-                  avatarWidth={ smallSquare }
-                  avatarHeight={ smallSquare }
-                  cardClassName="open-card"
-                  headerClassName="open-card-header"
+                  height={ square }
+                  width={ square }
                   name={ props.name }>
           { props.children }
         </TileCard>
