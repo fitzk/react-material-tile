67fc58b13644d966a8e4316423826adcd6422f4d
fitzk <kayla.fitzsimmons@protonmail.com>
2017-02-08 22:35:38 -0800

    add card test and update gitignore

--- test/test-card.js
+++ test/test-card.js
@@ -11,16 +11,63 @@ import darkBaseTheme from "material-ui/styles/baseThemes/darkBaseTheme"
 import { Card } from "./dist/card"
 import { getStyleProps } from "./utils"
 
-test("Card style defaults to { width: '50vw', height: '50vh' }", t => {
+// // trivial
+// test("Card defaults to { width: "50vw", height: "50vh" }", t => {
+// 	const muiTheme = getMuiTheme(darkBaseTheme);
+// 	const wrapper = shallow(<Card title="Tile Card"
+// 											              subtitle="a component"
+// 											              tileHeight="200px"
+// 											              tileWidth="200px"
+// 											              name="card">
+// 													<div/>
+// 												</Card>, {  muiTheme: muiTheme });
+// 		let cardStyle = getStyleProps(wrapper);
+// 		let style = { width: "50vw", height: "50vh" };
+// 		t.deepEqual(cardStyle, style)
+// });
+//
+//
+// test("Card: { width: "50vw", height: "300px" }", t => {
+// 	const muiTheme = getMuiTheme(darkBaseTheme);
+// 	const wrapper = shallow(<Card title="Tile Card"
+// 											              subtitle="a component"
+// 											              tileHeight="200px"
+// 											              tileWidth="200px"
+// 																		cardHeight="300px"
+// 											              name="card">
+// 													<div/>
+// 												</Card>, {  muiTheme: muiTheme });
+// 		let cardStyle = getStyleProps(wrapper);
+// 		let style = { width: "50vw", height: "300px" };
+// 		t.deepEqual(cardStyle, style)
+// });
+//
+// test("Card: { width: "300px", height: "50vh" }", t => {
+// 	const muiTheme = getMuiTheme(darkBaseTheme);
+// 	const wrapper = shallow(<Card title="Tile Card"
+// 											              subtitle="a component"
+// 											              tileHeight="200px"
+// 											              tileWidth="200px"
+// 																		cardWidth="300px"
+// 											              name="card">
+// 													<div/>
+// 												</Card>, {  muiTheme: muiTheme });
+// 		let cardStyle = getStyleProps(wrapper);
+// 		let style = { width: "300px", height: "50vh" };
+// 		t.deepEqual(cardStyle, style)
+// });
+
+test("Card style", t => {
 	const muiTheme = getMuiTheme(darkBaseTheme);
 	const wrapper = shallow(<Card title="Tile Card"
 											              subtitle="a component"
 											              tileHeight="200px"
 											              tileWidth="200px"
+																		cardStyle={{ height: "400px"}}
 											              name="card">
 													<div/>
 												</Card>, {  muiTheme: muiTheme });
 		let cardStyle = getStyleProps(wrapper);
-		let style = { width: '50vw', height: '50vh' };
+		let style = { width: "50vw", height: "400px" };
 		t.deepEqual(cardStyle, style)
 });
